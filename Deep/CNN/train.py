67841dfb55c7d5849950a3af8ca0987166a21243
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-02-08 14:04:00 +0200

    neuro net

--- Deep/CNN/train.py
+++ Deep/CNN/train.py
@@ -6,4 +6,14 @@ def train(model, train_loader, cost, optimizer, epoch):
     model.train()
     for e in range(epoch):
         running_loss = 0
-        
+        correct=0
+        for data, target in train_loader:
+            optimizer.zero_grad()
+            pred = model(data)
+            loss = cost(pred, target)
+            running_loss += loss
+            loss.backward()
+            optimizer.step()
+            pred=pred.argmax()
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    print(f"Epoch {e}: Loss {running_loss/len(train_loader.dataset)}, Accuracy {100*(correct/len(train_loader.dataset))}%")    
